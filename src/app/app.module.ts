e65309307943769f175bbea7cca99513921fee19
RafaelPerez de Lima <dr.rafalima@hotmail.com>
2020-01-13 23:33:07 -0300

    Update componets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
 
-import { AppComponent } from './app.component'
+import { AppComponent } from './app.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
